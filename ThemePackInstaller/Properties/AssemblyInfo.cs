59f75c435916738ec3219f72337fa602467a3082
SeppPenner <HansM2013@gmx.de>
2019-10-27 22:50:08 +0100

    Updated nuget packages, added GitVersionTask.

--- ThemePackInstaller/Properties/AssemblyInfo.cs
+++ ThemePackInstaller/Properties/AssemblyInfo.cs
@@ -23,17 +23,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("1301a99e-dd19-4f9c-9f48-6e9d83003dd4")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.0.0.2")]
-[assembly: AssemblyFileVersion("1.0.0.2")]
